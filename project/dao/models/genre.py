aed53ac16e01079b25fa33a7fc713dab19c55550
alexander <Avvrazek@gmail.com>
2022-02-14 21:40:52 +0300

    Changed files

--- project/dao/models/genre.py
+++ project/dao/models/genre.py
@@ -3,7 +3,7 @@ from project.setup_db import db
 
 
 class Genre(BaseMixin, db.Model):
-    __tablename__ = "genres"
+    __tablename__ = "genre"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
